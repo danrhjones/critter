504b696c0541c09066833a49b52efc3114bcf855
Dan Jones <dan.rh.jones@gmail.com>
2021-01-17 15:36:24 +0000

    initial commit

---
+++ src/main/java/com/udacity/jdnd/course3/critter/user/EmployeeDTO.java
@@ -0,0 +1,19 @@
+package com.udacity.jdnd.course3.critter.user;
+
+import java.time.DayOfWeek;
+import java.util.Set;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Represents the form that employee request and response data takes. Does not map
+ * to the database directly.
+ */
+@Getter
+@Setter
+public class EmployeeDTO {
+    private long id;
+    private String name;
+    private Set<EmployeeSkill> skills;
+    private Set<DayOfWeek> daysAvailable;
+}
